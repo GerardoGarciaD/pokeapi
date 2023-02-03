14d1618765b6c947b0d464b86ff7ddd1acaf1098
Gerardo <geraz26@hotmail.com>
2023-02-03 08:33:56 -0600

    Fetching information from PokeApi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,41 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
 import { MainLayout } from '../components/layouts';
-import { Box } from '@mui/system';
+import pokeApi from '../api/pokeApi';
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
+import Grid from '@mui/material/Grid';
+import { PokemonCard } from '../components/pokemon';
 
-const Home: NextPage = () => {
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const Home: NextPage<Props> = ({ pokemons }) => {
   return (
     <MainLayout title="Index PokeApi">
-      <Box>PokeAPI</Box>
+      <Grid container spacing={2}>
+        {pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon}></PokemonCard>
+        ))}
+      </Grid>
     </MainLayout>
   );
 };
 
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=251');
+
+  const pokemons: SmallPokemon[] = data.results.map((poke, id) => ({
+    ...poke,
+    id: id + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+      id + 1
+    }.svg`,
+  }));
+
+  return {
+    props: {
+      pokemons,
+    },
+  };
+};
 export default Home;
