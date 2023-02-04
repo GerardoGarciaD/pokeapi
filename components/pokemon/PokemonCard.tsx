97f8e423a1f0afbb9b44e42a9a90afd911b23258
Gerardo <geraz26@hotmail.com>
2023-02-03 20:31:43 -0600

    Creater router to go to the pokemon info

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -8,17 +8,24 @@ import {
   Typography,
 } from '@mui/material';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 interface Props {
   pokemon: SmallPokemon;
 }
 
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(`/pokemon/${pokemon.id}`);
+  };
+
   const { id, name, img } = pokemon;
   return (
     <Grid item key={id} xs={6} sm={3} md={2} xl={1}>
       <Card>
-        <CardActionArea>
+        <CardActionArea onClick={onClick}>
           <CardContent>
             <Typography> {name} </Typography>
             <Image src={img} width={100} height={100} alt={name}></Image>
