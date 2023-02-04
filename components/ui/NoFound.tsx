2795657defe97bae37167b9438847dde9da49624
Gerardo <geraz26@hotmail.com>
2023-02-04 12:52:12 -0600

    Adding component to display when no pokemon is found

---
+++ components/ui/NoFound.tsx
@@ -0,0 +1,16 @@
+import { Box } from '@mui/system';
+import React from 'react';
+
+export const NoFound = () => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      height="80vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      No Hay favoritos
+    </Box>
+  );
+};
