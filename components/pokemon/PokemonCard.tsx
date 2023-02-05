90356aeb5ccad76e3e67937eed81799115ae33ff
Gerardo <geraz26@hotmail.com>
2023-02-04 19:22:08 -0600

    Updating info of pokemons and dynamic styles

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,5 +1,4 @@
-import React, { FC } from 'react';
-import { SmallPokemon } from '../../interfaces/pokemon-list';
+import React, { FC, useState } from 'react';
 import {
   Grid,
   Card,
@@ -9,28 +8,67 @@ import {
 } from '@mui/material';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import styles from '../../styles/pokemonCard.module.css';
+import { PokemonV2Pokemon } from '../../interfaces/pokemon-graphql';
+import Button from '@mui/material/Button';
+import { typeColors } from '../../interfaces';
 
 interface Props {
-  pokemon: SmallPokemon;
+  pokemon: PokemonV2Pokemon;
 }
 
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
   const router = useRouter();
+  const { id, name, img, pokemon_v2_pokemontypes } = pokemon;
+  const pokemonType = pokemon_v2_pokemontypes[0].pokemon_v2_type.name;
 
   const onClick = () => {
-    router.push(`/name/${pokemon.name}`);
+    router.push(`/name/${name}`);
   };
 
-  const { id, name, img } = pokemon;
   return (
-    <Grid item key={id} xs={6} sm={3} md={2} xl={1}>
-      <Card>
-        <CardActionArea onClick={onClick}>
-          <CardContent>
-            <Typography> {name} </Typography>
-            <Image src={img} width={100} height={100} alt={name}></Image>
-          </CardContent>
-        </CardActionArea>
+    <Grid item key={id} xs={6} sm={4} lg={3} xl={2}>
+      <Card
+        className={styles['card']}
+        style={{
+          background: `radial-gradient(circle at 50% 0%,${
+            typeColors[pokemonType as keyof typeof typeColors]
+          } 36%, #ffffff 36%)`,
+        }}
+      >
+        <CardContent>
+          <CardActionArea onClick={onClick}>
+            <Image
+              className={styles.pokemonImg}
+              src={img}
+              width={150}
+              height={150}
+              alt={name}
+            ></Image>
+            <Typography className={styles.capitalize} textAlign="center">
+              {name}
+            </Typography>
+          </CardActionArea>
+          <div className={styles.type}>
+            {pokemon.pokemon_v2_pokemontypes.map((type) => {
+              const pokemonType = type.pokemon_v2_type.name;
+              return (
+                <Button
+                  className={styles.capitalize}
+                  style={{
+                    background: `${
+                      typeColors[pokemonType as keyof typeof typeColors]
+                    }`,
+                    color: 'white',
+                  }}
+                  key={pokemonType}
+                >
+                  {pokemonType}
+                </Button>
+              );
+            })}
+          </div>
+        </CardContent>
       </Card>
     </Grid>
   );
