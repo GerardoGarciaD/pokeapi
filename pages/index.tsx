6bc634a0846c8c65ffd4250c13f037bcfeb8cffa
Gerardo <geraz26@hotmail.com>
2023-02-04 19:28:07 -0600

    Call to the poke api using graphQL

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import type { GetStaticProps, NextPage } from 'next';
 import { MainLayout } from '../components/layouts';
-import pokeApi from '../api/pokeApi';
-import { PokemonListResponse, SmallPokemon } from '../interfaces';
 import Grid from '@mui/material/Grid';
 import { PokemonCard } from '../components/pokemon';
-
+import client from '../apollo-client';
+import { gql } from '@apollo/client/core';
+import { AllPokemons, PokemonV2Pokemon } from '../interfaces/pokemon-graphql';
 interface Props {
-  pokemons: SmallPokemon[];
+  pokemons: PokemonV2Pokemon[];
 }
 
 const Home: NextPage<Props> = ({ pokemons }) => {
+  console.log(pokemons);
+
   return (
     <MainLayout title="Index PokeApi">
       <Grid container spacing={2}>
@@ -22,19 +24,41 @@ const Home: NextPage<Props> = ({ pokemons }) => {
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=251');
+  const { data } = await client.query<AllPokemons>({
+    query: gql`
+      query GetPokemonsInfo {
+        pokemon_v2_pokemon(limit: 251) {
+          name
+          id
+          pokemon_v2_pokemontypes {
+            pokemon_v2_type {
+              name
+            }
+          }
+          pokemon_v2_pokemonstats {
+            base_stat
+            pokemon_v2_stat {
+              name
+            }
+          }
+        }
+      }
+    `,
+  });
 
-  const pokemons: SmallPokemon[] = data.results.map((poke, id) => ({
-    ...poke,
-    id: id + 1,
-    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-      id + 1
-    }.svg`,
-  }));
+  const pokemons: PokemonV2Pokemon[] = data.pokemon_v2_pokemon.map(
+    (poke, id) => ({
+      ...poke,
+      id: id + 1,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+        id + 1
+      }.svg`,
+    })
+  );
 
   return {
     props: {
-      pokemons,
+      pokemons: pokemons,
     },
   };
 };
