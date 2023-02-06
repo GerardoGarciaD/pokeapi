35f20bd717dcecc313e587b35c381598643e39cd
Gerardo <geraz26@hotmail.com>
2023-02-06 08:30:18 -0600

    Wrapping app with the graphql client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,17 @@ import '../styles/globals.css';
 import { ThemeProvider } from '@emotion/react';
 import { lightTheme } from '../themes/';
 import { CssBaseline } from '@mui/material';
+import client from '../apollo-client';
+import { ApolloProvider } from '@apollo/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline></CssBaseline>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline></CssBaseline>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
